606d3aef891e7b8521b38bb4c9156be1a19e36c1
Jrod7938 <Jrod7938@gmail.com>
2024-08-16 03:42:49 -0400

    correcting build file so actions can run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
 }
 
 group = "com.gepc"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.gepc.MainKt")
+}
 
 repositories {
     mavenCentral()
