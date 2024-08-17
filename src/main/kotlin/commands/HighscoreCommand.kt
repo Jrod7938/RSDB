80a3e2c4b5dacd7454285a179f4f77ffbbaead29
Jrod7938 <Jrod7938@gmail.com>
2024-08-16 21:08:57 -0400

    - hotfix to format numbers properly

--- src/main/kotlin/commands/HighscoreCommand.kt
+++ src/main/kotlin/commands/HighscoreCommand.kt
@@ -5,6 +5,8 @@ import com.gepc.models.Skill
 import com.gepc.service.HighscoreService
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import java.text.NumberFormat
+import java.util.*
 
 object HighscoreCommand {
     suspend fun handle(event: ChatInputCommandInteractionCreateEvent) {
@@ -20,13 +22,20 @@ object HighscoreCommand {
     }
 
     private fun formatHighscoreResponse(playerName: String, highscoreEntries: Map<Skill, HighscoreEntry>?): String {
+        val numberFormat = NumberFormat.getNumberInstance(Locale.US)
         return if (highscoreEntries.isNullOrEmpty()) {
             "Could not fetch highscores for player: $playerName"
         } else {
             val builder = StringBuilder("```kotlin\n")
             builder.append("Highscores for $playerName:\n")
             highscoreEntries.forEach { (skill, entry) ->
-                builder.append("${skill.displayName} - Rank: ${entry.rank}, Level: ${entry.level}, Experience: ${entry.experience}\n")
+                builder.append(
+                    "${skill.displayName} - Rank: ${numberFormat.format(entry.rank)}, Level: ${
+                        numberFormat.format(
+                            entry.level
+                        )
+                    }, Experience: ${numberFormat.format(entry.experience)}\n"
+                )
             }
             builder.append("```")
             builder.toString()
