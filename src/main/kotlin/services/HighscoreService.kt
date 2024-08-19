522d94b86beccc2909715c3f5bc8d7fc6142ac10
Jrod7938 <Jrod7938@gmail.com>
2024-08-18 20:13:20 -0400

    - updated files with copyright

--- src/main/kotlin/services/HighscoreService.kt
+++ src/main/kotlin/services/HighscoreService.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the RuneScape Discord Bot project.
+ *
+ * Licensed under the MIT License. You may obtain a copy of the License at
+ * https://opensource.org/licenses/MIT
+ *
+ * © 2024 Jancarlos Rodriguez, Omar Rodriguez
+ */
+
 package com.rsdb.services
 
 import com.rsdb.models.HighscoreEntry
